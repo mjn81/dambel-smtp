eb46241245dd31190c2f26dbee823f0851684f75
mjn81 <mjjnn81@gmail.com>
2023-07-11 21:25:09 +0330

    [ADD] reset and verify both complete.

--- constants/index.js
+++ constants/index.js
@@ -14,9 +14,19 @@ exports.getEnv = (key) => {
 }
 
 exports.getOtpEmailContent = ({code, name}) => {
-  return `<h1>Hi ${name},</h1><br />
+  return `<h2>Hi ${name},</h2><br />
 <p>There was a request to change your password!<br/>
 If you did not make this request then please ignore this email.<br/>
 Otherwise, please back to the website and complete your reset password.</p><br/>
 <strong>Verification Code : ${code}</strong>`;
+}
+
+exports.getVerifyEmailContent = ({ code, name }) => {
+  return `<h2>Hi ${name},</h2><br />
+<strong>Verification Code : ${code}</strong>
+<p>Enter this code in our website to activate your account.</p><br />
+<p>If you have any questions, send us an email dambelcorp@outlook.com.</p><br />
+<p>We’re glad you’re here!</p><br />
+<p>The Dambel team</p>
+`;
 }
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const Yup = require('yup');
 const getMailer = require('./services/mail').getInstance;
-const { PORT, getOtpEmailContent, getEnv } = require('./constants');
+const { PORT, getOtpEmailContent: getResetEmailContent, getEnv, getVerifyEmailContent } = require('./constants');
 
 const sendEmail = async (to, subject, html) => {
 	const mailer = getMailer();
@@ -25,7 +25,7 @@ const reqSchema = Yup.object({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/api/v1/email/otp', async (req, res) => {
+app.post('/api/v1/email/verify', async (req, res) => {
 	// send email with nodemailer
 	try {
 		const { email, name, code } = req.body;
@@ -33,22 +33,26 @@ app.post('/api/v1/email/otp', async (req, res) => {
 		// send email
 		sendEmail(
 			email,
-			'Dambel One time password',
-			getOtpEmailContent({ code, name })
+			'Verify Account',
+			getVerifyEmailContent({ code, name })
 		);
-		return res.status(200).json({ message: 'Email sent' });
+		return res.status(201).json({ message: 'Email sent' });
 	} catch (error) {
 		return res.status(400).json({ message: error.message });
 	}
 });
 
-app.post('/api/v1/email/reset', (req, res) => {
-	const { email, name, code } = req.body;
-	// send email with nodemailer
+app.post('/api/v1/email/reset', async (req, res) => {
 	try {
-		reqSchema.validate({ email, name });
-		// send emai
-		return res.status(200).json({ message: 'Email sent' });
+		const { email, name, code } = req.body;
+		await reqSchema.validate({ email, name });
+		// send email
+		sendEmail(
+			email,
+			'Reset password',
+			getResetEmailContent({ code, name })
+		);
+		return res.status(201).json({ message: 'Email sent' });
 	} catch (error) {
 		return res.status(400).json({ message: error.message });
 	}
@@ -59,6 +63,10 @@ app.get('/api', (req, res) => {
 		message: 'Welcome to Dambel Smtp API',
 	});
 });
+
+app.get('/', (req, res) => {
+	return res.send(`Welcome to Dambel Smtp!!`);
+})
 
 app.listen(PORT, () => {
 	console.log(`[MESSAGE] Server listening on port ${PORT}`)
